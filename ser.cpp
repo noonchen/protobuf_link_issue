090692b9d96bdb3a22e2f3e6bae29a206e70bfa6
noonchen <chennoon233@foxmail.com>
2026-01-14 04:51:04 +0000

    update

--- ser.cpp
+++ ser.cpp
@@ -1,16 +1,33 @@
 #include "ser.h"
 #include "test.pb.h"
 #include <string>
+#include <iostream>
 
 extern "C" SER_API size_t ser(const char* msg) {
     if (!msg) return 0;
 
-    test::RetStatus status;
-    status.set_success(true);
-    status.set_message(msg);
+    test::RetStatus status1;
+    status1.set_success(true);
+    status1.set_message("static str");
+
+    std::string tmp;
+    status1.SerializeToString(&tmp);
+    std::cout << tmp.size() << std::endl;
+
+    test::RetStatus status2;
+    status2.set_success(true);
+    status2.set_message(std::string("std string"));
+
+    tmp.clear();
+    status2.SerializeToString(&tmp);
+    std::cout << tmp.size() << std::endl;
+
+    test::RetStatus status3;
+    status3.set_success(true);
+    status3.set_message(msg);
 
     std::string out;
-    if (status.SerializeToString(&out)) {
+    if (status3.SerializeToString(&out)) {
         return out.size();
     }
     return 0;
